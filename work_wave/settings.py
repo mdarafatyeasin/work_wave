c938efdc6c0688d9fb6f91cc7d8515497910e415
mdarafatyeasin <yeasinarafat54239@gmail.com>
2024-02-26 19:22:21 +0600

    allow methods

--- work_wave/settings.py
+++ work_wave/settings.py
@@ -127,8 +127,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
 
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
